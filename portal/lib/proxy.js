4d9ba9e53c20e7254aa94174deb5cabf1add3a88
Steve Birk <stevebirk@gmail.com>
2011-07-04 11:26:02 -0400

    move proxy to new http.request syntax, move .AddListener to new .on syntax

--- portal/lib/proxy.js
+++ portal/lib/proxy.js
@@ -23,40 +23,55 @@ A simple proxy for CouchDB requests to a configured CouchDB host/port
     res: express.js response object
     dataHandler : callback that takes proxied response Content-Length 
 */
-module.exports.couchdb_proxy = function(target, req, res, outDataHandler, inDataHandler){
+module.exports.couchdb_proxy = function(    auth, 
+                                            target, 
+                                            req, 
+                                            res, 
+                                            outDataHandler, 
+                                            inDataHandler) {
     
     // get db name if it exists in target
     var db = module.exports.get_db_name(target);
-
-    // create an HTTP proxy client to CouchDB
-    var proxy_client = http.createClient(config.couchDBPort, config.couchDBHost); 
+    
+    // request options
+    var options = {
+        host : config.couchDBHost,
+        port : config.couchDBPort,
+        path : target,
+        method : req.method,
+        headers : req.headers
+    };
+    
+    // if not undefined insert auth into our proxy headers
+    if (auth !== undefined) {
+        options.headers.Authorization = auth;
+    }
+        
+    // make the desired request
+    var proxy_req = http.request(options);
     
     // if we get any kind of error currently return a 500 response
-    proxy_client.addListener('error', function(connectionException){
-        util.log('ERROR: connection with ' + proxy_client.host
-                    + ':' +proxy_client.port + '\n' + connectionException);
+    proxy_req.once('error', function(e){
+        util.log('problem with proxy: ' + e.message);
         // this should send a proper 500 error...
         res.writeHead(500, "Unable to reach database.");
         res.end()
     });
     
-    // make the desired request
-    var proxy_req = proxy_client.request(req.method, target, req.headers);
-    
     // create our response callback for the proxy request
-    proxy_req.addListener('response', function(proxy_res) {
+    proxy_req.on('response', function(proxy_res) {
         
         var data_out = 0;
                
         // write to our response in chunks
-        proxy_res.addListener("data", function(chunk) {
+        proxy_res.on("data", function(chunk) {
             data_out += Buffer.byteLength(''+chunk);
             res.write(chunk, 'binary');
         });
 
         // if we have a dataHandler callback, call it now
         // then end our response
-        proxy_res.addListener("end", function() {
+        proxy_res.on("end", function() {
             
             if (outDataHandler !== undefined)
                 outDataHandler(data_out, db);
@@ -71,13 +86,13 @@ module.exports.couchdb_proxy = function(target, req, res, outDataHandler, inData
     
     // if we have a streaming request, write it to our
     // proxy request
-    req.addListener('data', function(chunk) {
+    req.on('data', function(chunk) {
         data_in += Buffer.byteLength(''+chunk);
         proxy_req.write(chunk, 'binary');
     });
     
     // when we're done writing to the proxied request, end it
-    req.addListener('end', function() {
+    req.on('end', function() {
         
         if (inDataHandler !== undefined)
             inDataHandler(data_in, db);
